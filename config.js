ef6b2cc416bc355a18cd67affd180370831edd21
ARYAN0815 <vermuriaryan@gmail.com>
2022-01-16 11:56:50 +0530

    database config added

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBP3sotjHftBSkOnx34NEErwR3aPcLy5wc",
+    authDomain: "class-test-d5362.firebaseapp.com",
+    databaseURL: "https://class-test-d5362-default-rtdb.firebaseio.com",
+    projectId: "class-test-d5362",
+    storageBucket: "class-test-d5362.appspot.com",
+    messagingSenderId: "415179915202",
+    appId: "1:415179915202:web:0f476eea2f2df7e789205b"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
